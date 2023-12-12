04b3238bc90a8f95ab3da2eaf46a1eb9dc759e85
carloshoras <carloshoras>
2023-12-12 12:40:39 +0100

    Empezado saved url

--- js/script.js
+++ js/script.js
@@ -26,26 +26,61 @@ const characters = [
 
 buttonPassword.addEventListener('click', function () {
     if (inputPassword.value > 11 && inputPassword.value < 51) {
-        let password = ""
+        let password
         let characterType
-        let characterNumber
-        for (let i=0;i<inputPassword.value;i++) {
-            characterType = Math.floor(Math.random() * 4)
-            characterNumber = Math.floor(Math.random()*characters[characterType].length)
-            password += characters[characterType][characterNumber]
+        let characterIndex
+        let oneOfEachCharacters = false
+        //We check that it contains at least one character of each type
+        while (!oneOfEachCharacters) {
+            let checkOneOfEach = {}
+            password = ""
+            for (let i=0;i<inputPassword.value;i++) {
+                characterType = Math.floor(Math.random() * 4)
+                characterIndex = Math.floor(Math.random()*characters[characterType].length)
+                password += characters[characterType][characterIndex]
+                checkOneOfEach[characterType] = 1
+            }
+            //If the object 'checkOneOfEach' doesn't have 4 positions (one per type), repeat the process.
+            if (Object.keys(checkOneOfEach).length == 4) {
+                oneOfEachCharacters = true
+            }
         }
+        //We paint the password
         generatedPassword.innerHTML = `
         <p>Contraseña Generada:</p>
         <p>${password}</p>`
     } else {
-        alert("Mete un numero correcto")
+        alert("Introduce un numero entre 12 y 50")
     }
 })
-// -capturar boton
-// -addeventlistener al boton --> coge valor Input
-// ---si numero sale de rangoo , alerta por pantalla
 
-// -creamos elemento dentro del div que vaya al final, ahi metermos innerHTML
-// -mayusculas/minusculsa/numeros/simbolos
+////LISTADO DE LINKS
+const buttonAddUrl = document.querySelector(".inputLink button")
+const nameUrl = document.querySelector(".nameUrl")
+const url = document.querySelector(".url")
+const savedLinks = document.querySelector(".savedLinks")
 
-// -pintamos con innerhtml
+buttonAddUrl.addEventListener('click', function () {
+    console.log(nameUrl)
+    if (localStorage.getItem(nameUrl.value)) {
+        alert("Ya existe una url guardada con este mismo nombre")
+    } else if (localStorage.getItem(nameUrl.value) === url.value) {
+        alert("Esta url ya esta guardada con otro nombre")
+    } else {
+        localStorage.setItem((nameUrl.value),url.value)
+        savedLinks.innerHTML += `
+        <div id="${nameUrl.value}">
+            <button>X</button>
+            <a href="${url.value}">${nameUrl.value}</a>
+        </div>`
+    }
+})
+// -boton añadirenlace addeventlistener
+// ---comprobar si link ya existe alerta "ya guardaste"
+// ---local storage set item nombre : URL
+// ---pintar en el div "savedlinks" con un <a href> y con boton de eliminar
+
+// queryselectorall(todos los enlaces guardados)
+// queryselectorAll(todos los botones elimnar)
+// foreach --> addeventlistener --> con index teenemos el elemento y lo eliminamos
+// -->Eliminamos tambien del local storage a traves de un id="nombre" en el elemento?
