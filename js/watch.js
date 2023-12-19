7a17ce1e36c6eddb1ddd91b9143f709438f7ea9a
carloshoras <carloshoras>
2023-12-19 19:03:25 +0100

    url watch

--- js/watch.js
+++ js/watch.js
@@ -1,6 +1,5 @@
 const watch = document.querySelector(".watch")
-const nameFileHtml = location.pathname.split("/") // We get the html attached to the execution of the script
-if (nameFileHtml[nameFileHtml.length-1] == '' || nameFileHtml[nameFileHtml.length-1] == 'index.html') console.log(nameFileHtml)
+const nameFileHtml = location.pathname.split("/")[nameFileHtml.length-1] // We get the html attached to the execution of the script
 
 function sentenceHour(hour) {
     if (hour>0 && hour<7) return "Es hora de descansar. Apaga y sigue mañana"
@@ -18,7 +17,7 @@ function pintaHoraFecha () {
     <p class="time">${fecha.getHours().toString().padStart(2,'0')}:${fecha.getMinutes().toString().padStart(2,'0')}:${fecha.getSeconds().toString().padStart(2,'0')}</p>
     <p class="date">${fecha.getDate().toString().padStart(2,'0')}/${(fecha.getMonth()+1).toString().padStart(2,'0')}/${fecha.getFullYear()}</p>`
     //If we are in the watch dedicated page --> Include an intro and motivational sentence
-    if (nameFileHtml === "index.html" || location.pathname === "/") {
+    if (nameFileHtml === "index.html" || nameFileHtml === "") {
         watch.innerHTML = timeDateHtml
     } else {
         watch.innerHTML = "<span>Son las...</span>" + timeDateHtml + `<span>${sentenceHour(fecha.getHours())}</span>`
